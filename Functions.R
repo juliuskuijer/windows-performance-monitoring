b0d297fb873d10be58cabfa8d20c3212f9189333
juliuskuijer <juliuskuijer@hotmail.com>
2016-03-31 14:28:22 -0700

    Added files via upload

---
+++ Functions.R
@@ -0,0 +1,104 @@
+function(..., plotlist=NULL, file, cols=1, layout=NULL) {
+  require(grid)
+  
+  # Make a list from the ... arguments and plotlist
+  plots <- c(list(...), plotlist)
+  
+  numPlots = length(plots)
+  
+  # If layout is NULL, then use 'cols' to determine layout
+  if (is.null(layout)) {
+    # Make the panel
+    # ncol: Number of columns of plots
+    # nrow: Number of rows needed, calculated from # of cols
+    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
+                     ncol = cols, nrow = ceiling(numPlots/cols))
+  }
+  
+  if (numPlots==1) {
+    print(plots[[1]])
+    
+  } else {
+    # Set up the page
+    grid.newpage()
+    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
+    
+    # Make each plot, in the correct location
+    for (i in 1:numPlots) {
+      # Get the i,j matrix positions of the regions that contain this subplot
+      matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
+      
+      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
+                                      layout.pos.col = matchidx$col))
+    }
+  }
+}
+
+FormatSI <- function(x, ...) {
+  # Format a vector of numeric values according to the
+  # International System of Units.
+  # http://en.wikipedia.org/wiki/SI_prefix
+  #
+  # Args:
+  #   x  : A vector of numeric values
+  #   ...: Remaining args passed to format()
+  #
+  # Returns:
+  #   A vector of strings using SI prefix notation
+  #
+  # Bugs:
+  #   Does not (yet) work with small (<1) numbers
+  #
+  scale.frac <- 1000
+  scale.unit <- c("k", "M", "G", "T", "P", "E", "Z", "Y")
+  
+  # Start with empty prefixes
+  p <- rep(" ", length(x))
+  
+  # Divide by scale.frac and store scale.unit if value is
+  # large enough. Repeat for all units.
+  for(i in 1:length(scale.unit)) {
+    p[x >= scale.frac] <- scale.unit[i]
+    x[x >= scale.frac] <- x[x >= scale.frac] / scale.frac
+  }
+  
+  return(paste(format(round(x,1), trim=TRUE, scientific=FALSE, ...), p))
+}
+
+
+format_si <- function(...) {
+  # Format a vector of numeric values according
+  # to the International System of Units.
+  # http://en.wikipedia.org/wiki/SI_prefix
+  #
+  # Based on code by Ben Tupper
+  # https://stat.ethz.ch/pipermail/r-help/2012-January/299804.html
+  # Args:
+  #   ...: Args passed to format()
+  #
+  # Returns:
+  #   A function to format a vector of strings using
+  #   SI prefix notation
+  #
+  
+  function(x) {
+    limits <- c(1e-24, 1e-21, 1e-18, 1e-15, 1e-12,
+                1e-9,  1e-6,  1e-3,  1e0,   1e3,
+                1e6,   1e9,   1e12,  1e15,  1e18,
+                1e21,  1e24)
+    prefix <- c("y",   "z",   "a",   "f",   "p",
+                "n",   "µ",   "m",   " ",   "k",
+                "M",   "G",   "T",   "P",   "E",
+                "Z",   "Y")
+    
+    # Vector with array indices according to position in intervals
+    i <- findInterval(abs(x), limits)
+    
+    # Set prefix to " " for very small values < 1e-24
+    i <- ifelse(i==0, which(limits == 1e0), i)
+    
+    paste(format(round(x/limits[i], 1),
+                 trim=TRUE, scientific=FALSE, ...),
+          prefix[i])
+  }
+}
